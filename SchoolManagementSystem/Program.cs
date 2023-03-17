f7f33b0a615e4be76c2b559f10ec02fefd722878
Spas Milenkov <spasmilenkov123@gmail.com>
2023-03-17 11:24:24 +0200

    Add EF core and use MySql

--- SchoolManagementSystem/Program.cs
+++ SchoolManagementSystem/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using SchoolManagementSystem.Data;
+using ConfigurationManager = System.Configuration.ConfigurationManager;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,10 +10,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddDbContext<SchoolDbContext>(options => 
+    options.UseMySql(ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("SchoolDb"))));
+builder.Services.AddControllers();
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline.s
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
