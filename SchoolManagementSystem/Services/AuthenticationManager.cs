fc44de6a78d4cf1a7c4a28ddb204b2af132adcee
Spas Milenkov <spasmilenkov123@gmail.com>
2023-03-21 21:36:09 +0200

    Add AuthenticationManager.cs to separate authentication logic

---
+++ SchoolManagementSystem/Services/AuthenticationManager.cs
@@ -0,0 +1,51 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using Microsoft.IdentityModel.Tokens;
+using SchoolManagementSystem.Models;
+
+namespace SchoolManagementSystem.Services;
+
+public class AuthenticationManager
+{
+    private readonly IConfiguration _config;
+
+    public AuthenticationManager(IConfiguration configuration)
+    {
+        _config = configuration;
+    }
+    public void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+    {
+        using (var hmac = new HMACSHA512())
+        {
+            passwordHash = hmac.Key;
+            passwordSalt = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+        }
+    }
+
+    public bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+    {
+        using (var hmac = new HMACSHA512(passwordSalt))
+        {
+            var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            return computedHash.SequenceEqual(passwordHash);
+        }
+    }
+
+    public string CreateAuthenticationToken(LoginUserDto user)
+    {
+        List<Claim> claims = new List<Claim>()
+        {
+            new Claim(ClaimTypes.Email, user.Email),
+        };
+        var key = new SymmetricSecurityKey(
+            System.Text.Encoding.UTF8.GetBytes(_config.GetSection("Authentication:Token").Value));
+        var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+        var token = new JwtSecurityToken(
+            claims: claims,
+            expires: DateTime.Now.AddHours(2),
+            signingCredentials: credentials);
+        var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+        return jwt;
+    }
+}
