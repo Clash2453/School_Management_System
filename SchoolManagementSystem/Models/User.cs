e07ae07ae0e9ac62e714b59993773c6db10ef816
Spas Milenkov <spasmilenkov123@gmail.com>
2023-03-21 21:37:22 +0200

    Change the User entity so that I can hash the password in the database

--- SchoolManagementSystem/Models/User.cs
+++ SchoolManagementSystem/Models/User.cs
@@ -10,5 +10,6 @@ public class User
     public int UserId { get; set; }
     public string Name { get; set; } = null!;
     public string Email { get; set; } = null!;
-    public string Password { get; set; } = null!;
+    public byte[] Password { get; set; } = null!;
+    public byte[] Salt { get; set; } = null!;
 }
