cad69b43c993dc00a04f84a1ccc3b63718ebe59b
Spas Milenkov <spasmilenkov123@gmail.com>
2023-03-23 21:18:41 +0200

    Import icons

--- UI/src/main.js
+++ UI/src/main.js
@@ -4,8 +4,14 @@ import router from './router'
 
 import './assets/main.css'
 
+import { OhVueIcon, addIcons } from 'oh-vue-icons'
+import { BiGithub, BiFacebook, BiLinkedin, MdSchoolOutlined } from 'oh-vue-icons/icons'
+
+addIcons(BiGithub, BiFacebook, BiLinkedin, MdSchoolOutlined)
+
 const app = createApp(App)
 
+app.component('v-icon', OhVueIcon)
 app.use(router)
 
 app.mount('#app')
