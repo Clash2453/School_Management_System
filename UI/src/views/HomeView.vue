506a0820cb9735ab5ed11efe2067d5c27bd37ec1
Spas Milenkov <spasmilenkov123@gmail.com>
2023-03-23 21:16:14 +0200

    Add home view

---
+++ UI/src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<script>
+import FeaturesSectionComponent from '../components/FeaturesSectionComponent.vue'
+import HeroSection from '../components/HeroSectionComponent.vue'
+export default {
+  data: () => {
+    return {}
+  },
+  components: {
+    HeroSection,
+    FeaturesSectionComponent
+  }
+}
+</script>
+
+<template>
+  <HeroSection></HeroSection>
+  <FeaturesSectionComponent></FeaturesSectionComponent>
+</template>
