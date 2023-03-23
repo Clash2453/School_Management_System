0008b836bf6a4ca68ff245f76a9497673f19ce94
Spas Milenkov <spasmilenkov123@gmail.com>
2023-03-23 21:26:14 +0200

    Add data transfer objects for the user

--- SchoolManagementSystem/Models/LoginUserDto.cs
+++
@@ -1,7 +0,0 @@
-namespace SchoolManagementSystem.Models;
-
-public class LoginUserDto
-{
-    public string Email { get; set; } = string.Empty;
-    public string Password { get; set; } = string.Empty;
-}
