21f6b6e444881ea6305c42c680798801d120b369
Spas Milenkov <spasmilenkov123@gmail.com>
2023-04-04 22:14:48 +0300

    Fix the height of the login view

--- UI/src/views/LoginView.vue
+++ UI/src/views/LoginView.vue
@@ -49,6 +49,7 @@ export default {
   align-items: center;
   justify-content: center;
   color: white;
+  height: calc(100vh - 13rem);
 }
 .main-title {
   text-align: center;
@@ -62,7 +63,7 @@ export default {
   flex-direction: column;
   width: 100%;
   height: 100%;
-  min-height: 100vh;
+  /* min-height: 100vh; */
   padding: 2rem;
 }
 .subtitle {
@@ -75,6 +76,7 @@ export default {
 @media (max-width: 1250px) {
   .flex-container {
     flex-direction: column-reverse;
+    height: 100%;
   }
 }
 </style>
