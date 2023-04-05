76fcbb06fce99d2e131ef4d3743d4abb711e1206
Spas Milenkov <spasmilenkov123@gmail.com>
2023-04-05 22:33:35 +0300

    Add dto for the user login

---
+++ SchoolManagementSystem/Models/DataTransferObjects/LoginUserDto.cs
@@ -0,0 +1,7 @@
+namespace SchoolManagementSystem.Models.DataTransferObjects;
+
+public class LoginUserDto
+{
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
