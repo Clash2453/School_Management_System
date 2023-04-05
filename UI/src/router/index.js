a6bdc66b912a32a3666eac180c7644dbe53336b0
Spas Milenkov <spasmilenkov123@gmail.com>
2023-04-05 22:30:02 +0300

    Update router paths

--- UI/src/router/index.js
+++ UI/src/router/index.js
@@ -1,8 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+import Home from '../views/HomeView.vue'
+import Login from '../views/LoginView.vue'
+import Dashboard from '../views/DashboardView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: []
+  routes: [
+    { path: '/', component: Home },
+    { path: `/dashboard`, component: Dashboard },
+    { path: '/Login', component: Login }
+  ]
 })
 
 export default router
