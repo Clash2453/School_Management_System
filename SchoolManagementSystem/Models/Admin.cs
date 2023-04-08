f3c2f5c7ab59448e193c9e5f002527e4441f1316
Spas Milenkov <spasmilenkov123@gmail.com>
2023-04-08 20:36:49 +0300

    Add constructor to the admin class

--- SchoolManagementSystem/Models/Admin.cs
+++ SchoolManagementSystem/Models/Admin.cs
@@ -1,8 +1,12 @@
 using System.ComponentModel.DataAnnotations.Schema;
+using SchoolManagementSystem.Models.DataTransferObjects;
 
 namespace SchoolManagementSystem.Models;
 [Table( "Admins" )]
 public class Admin : User
 {
-    
+    public Admin(AdminDto request, User user):base(user)
+    {
+        
+    }
 }
