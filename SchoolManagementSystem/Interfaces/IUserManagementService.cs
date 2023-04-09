41346e43fdc99811a786a69fae4548d906890b8a
Spas Milenkov <spasmilenkov123@gmail.com>
2023-04-09 21:02:33 +0300

    Create the interface for the specialization Service

---
+++ SchoolManagementSystem/Interfaces/IUserManagementService.cs
@@ -0,0 +1,14 @@
+using SchoolManagementSystem.Enums;
+using SchoolManagementSystem.Models;
+using SchoolManagementSystem.Models.DataTransferObjects;
+
+namespace SchoolManagementSystem.Interfaces;
+
+public interface IUserManagementService
+{
+    public Task<User?> UserExists(int id);
+    public Task<Status> CreateStudent(StudentDto request);
+    public Task<Status> CreateTeacher(TeacherDto request);
+    public Task<Status> CreateAdmin(AdminDto request);
+    public Task<Status> DeleteUser(int id);
+}
