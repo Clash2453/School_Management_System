d122ddbd788070550a7dd5f327e9e474828168bc
Spas Milenkov <spasmilenkov123@gmail.com>
2023-05-19 19:22:41 +0300

    Add grade dto

--- SchoolManagementSystem/Models/DataTransferObjects/GradeDto.cs
+++ SchoolManagementSystem/Models/DataTransferObjects/GradeDto.cs
@@ -2,7 +2,8 @@ namespace SchoolManagementSystem.Models.DataTransferObjects;
 
 public class GradeDto
 {
-    public decimal Value { get; set; }
+    public float Value { get; set; }
+    public DateTime Date = DateTime.Now;
     public int StudentId { get; set; }
     public int TeacherId { get; set; }
 }
