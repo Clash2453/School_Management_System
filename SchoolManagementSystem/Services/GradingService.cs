ce799a7f6db5907ac26796abab4a08e7de3e1746
Spas Milenkov <spasmilenkov123@gmail.com>
2023-05-19 19:22:56 +0300

    Add grading service

--- SchoolManagementSystem/Services/GradingService.cs
+++ SchoolManagementSystem/Services/GradingService.cs
@@ -1,6 +1,8 @@
+using Microsoft.EntityFrameworkCore;
 using SchoolManagementSystem.Data;
 using SchoolManagementSystem.Enums;
 using SchoolManagementSystem.Interfaces;
+using SchoolManagementSystem.Models;
 using SchoolManagementSystem.Models.DataTransferObjects;
 
 namespace SchoolManagementSystem.Services;
@@ -14,21 +16,43 @@ public class GradingService : IGradingService
     }
     public async Task<Status> AddGrade(GradeDto request)
     {
+        var student = await _context.Students.FindAsync(request.StudentId);
+        var teacher = await _context.Teachers.FindAsync(request.TeacherId);
+        if (student == null || teacher == null)
+            return Status.Fail;
+        var grade = new Grade()
+        {
+            Value = request.Value,
+            Grader = teacher,
+            Owner = student,
+            Date = DateTime.Now,
+        };
+        _context.Grades.Add(grade);
+        await _context.SaveChangesAsync();
         return Status.Success;
     }
     
     public async Task<Status> DeleteGrade(int gradeId)
     {
+        var grade = await _context.Grades.FindAsync(gradeId);
+        if (grade == null)
+            return Status.Fail;
+        _context.Grades.Remove(grade);
+        await _context.SaveChangesAsync();
         return Status.Success;
     }
 
-    public async Task<Status> UpdateGrade(int gradeId)
+    public async Task<Status> UpdateGrade(int gradeId, GradeDto request)
     {
+        var grade = await _context.Grades.FindAsync(gradeId);
+        if (grade == null)
+            return Status.Fail;
+        
         return Status.Success;
     }
 
-    public async Task<Status> GetGRades(int studentId)
+    public async Task<List<Grade>> GetGrades(int requestedId)
     {
-        return Status.Success;
+        return await _context.Grades.Where(g => g.Owner.StudentId == requestedId).ToListAsync();
     }
 }
