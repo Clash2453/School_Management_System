e57b3d16acecb5d3da2e15c298c1d3e58714dc0e
Spas Milenkov <spasmilenkov123@gmail.com>
2023-05-19 19:31:57 +0300

    Fix the subject relations in the database

---
+++ SchoolManagementSystem/Migrations/20230519073818_AbsenceTableAndGradeDate.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SchoolManagementSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class AbsenceTableAndGradeDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Course",
+                table: "Subjects");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Date",
+                table: "Grade",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.CreateTable(
+                name: "Absences",
+                columns: table => new
+                {
+                    AbsenceId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    StudentId = table.Column<int>(type: "int", nullable: false),
+                    TeacherId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Absences", x => x.AbsenceId);
+                    table.ForeignKey(
+                        name: "FK_Absences_Students_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Students",
+                        principalColumn: "StudentId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Absences_Teacher_TeacherId",
+                        column: x => x.TeacherId,
+                        principalTable: "Teacher",
+                        principalColumn: "TeacherId",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Absences_StudentId",
+                table: "Absences",
+                column: "StudentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Absences_TeacherId",
+                table: "Absences",
+                column: "TeacherId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Absences");
+
+            migrationBuilder.DropColumn(
+                name: "Date",
+                table: "Grade");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Course",
+                table: "Subjects",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
