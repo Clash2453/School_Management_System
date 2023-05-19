5d0e7049eaa862819185f2f6c7ebab3a747cb624
Spas Milenkov <spasmilenkov123@gmail.com>
2023-05-19 19:21:45 +0300

    Add grade service interface

--- SchoolManagementSystem/Interfaces/IGradingService.cs
+++ SchoolManagementSystem/Interfaces/IGradingService.cs
@@ -1,4 +1,5 @@
 using SchoolManagementSystem.Enums;
+using SchoolManagementSystem.Models;
 using SchoolManagementSystem.Models.DataTransferObjects;
 
 namespace SchoolManagementSystem.Interfaces;
@@ -9,6 +10,6 @@ public interface IGradingService
 
     public Task<Status> DeleteGrade(int gradeId);
 
-    public Task<Status> UpdateGrade(int gradeId);
-    public Task<Status> GetGRades(int studentId);
+    public Task<Status> UpdateGrade(int gradeId, GradeDto request);
+    public Task<List<Grade>> GetGrades(int requestId);
 }
