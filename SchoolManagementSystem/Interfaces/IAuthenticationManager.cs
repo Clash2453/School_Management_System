97d3a97445fbfe2822ef9334af7dcf89d482bc7c
Spas Milenkov <spasmilenkov123@gmail.com>
2023-05-21 19:39:26 +0300

    Add the IAuthenticationManager.cs interface

---
+++ SchoolManagementSystem/Interfaces/IAuthenticationManager.cs
@@ -0,0 +1,12 @@
+using SchoolManagementSystem.Enums;
+using SchoolManagementSystem.Models;
+using SchoolManagementSystem.Models.DataTransferObjects;
+
+namespace SchoolManagementSystem.Interfaces;
+
+public interface IAuthenticationManager
+{
+    public void CreatePasswordHash(string password, out string passwordHash, out string passwordSalt);
+    public bool VerifyPasswordHash(string password, string passwordHash);
+    public string CreateAuthenticationToken(LoginUserDto user, string role);
+}
