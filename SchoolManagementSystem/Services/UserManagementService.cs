e95ac2f91122c66229bc44f420f96ac8d7cb4782
Spas Milenkov <spasmilenkov123@gmail.com>
2023-05-21 19:37:54 +0300

    Move the creation service functions

--- SchoolManagementSystem/Services/UserManagementService.cs
+++ SchoolManagementSystem/Services/UserManagementService.cs
@@ -10,14 +10,45 @@ namespace SchoolManagementSystem.Services;
 public class UserManagementService : IUserManagementService
 {
     private SchoolDbContext _context;
-    public UserManagementService(SchoolDbContext context)
+    private static IAuthenticationManager _authManager;
+
+    public UserManagementService(SchoolDbContext context, IAuthenticationManager authManager)
     {
         _context = context;
+        _authManager = authManager;
     }
 
-    public Task<User?> UserExists(int id)
+    public async Task<User?> UserExists(int id)
     {
-        return _context.Users.FirstOrDefaultAsync(u => u.UserId == id);
+        return await _context.Users.FirstOrDefaultAsync(u => u.UserId == id);
+    }
+
+    public async Task<User?> AttemptLogin(LoginUserDto request)
+    {
+        var user = await _context.Users.FirstOrDefaultAsync(u=> u.Email == request.Email);
+        if (user == null)
+            return user;
+        if (!_authManager.VerifyPasswordHash(request.Password, user.Password))
+            return null;
+        
+        var encryptedToken = _authManager.CreateAuthenticationToken(request, user.Role);
+
+        return user;
+    }
+    public async Task<Status> CreateUser(UserDto request)
+    {
+        _authManager.CreatePasswordHash(request.Password, out string passwordHash, out string passwordSalt);
+        var user = new User
+        {
+            Email = request.Email,
+            Password = passwordHash,
+            Salt = passwordSalt,
+            Name = request.Name,
+            Role = "Guest"
+        };
+        _context.Users.Add(user);
+        await _context.SaveChangesAsync();
+        return Status.Success;
     }
     public async Task<Status> CreateStudent(StudentDto request)
     {
