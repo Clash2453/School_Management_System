02e88700d461d127a08a7c4929b861817481eb95
Spas Milenkov <spasmilenkov123@gmail.com>
2023-05-21 19:37:17 +0300

    Update the user controller to use dependency injection

--- SchoolManagementSystem/Controllers/UserController.cs
+++ SchoolManagementSystem/Controllers/UserController.cs
@@ -1,11 +1,8 @@
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using SchoolManagementSystem.Data;
+using SchoolManagementSystem.Enums;
+using SchoolManagementSystem.Interfaces;
 using SchoolManagementSystem.Models;
 using SchoolManagementSystem.Models.DataTransferObjects;
-using SchoolManagementSystem.Services;
-using AuthenticationManager = SchoolManagementSystem.Services.AuthenticationManager;
 
 namespace SchoolManagementSystem.Controllers;
 
@@ -13,31 +10,23 @@ namespace SchoolManagementSystem.Controllers;
 [ApiController]
 public class UserController : ControllerBase
 {
-    private readonly SchoolDbContext _context;
-    private readonly AuthenticationManager _authManager;
-    private readonly UserCreationService _userFactory;
-
-    public UserController(SchoolDbContext context, IConfiguration config)
+    private readonly IUserManagementService _userManagementService;
+    private readonly IAuthenticationManager _authManager;
+    public UserController(IUserManagementService userManagementService, IAuthenticationManager authManager)
     {
-        _context = context;
-        _authManager = new AuthenticationManager(config);
-        _userFactory = new UserCreationService(_authManager);
+        _userManagementService = userManagementService;
+        _authManager = authManager;
     }
 
     // GET: api/User/5
     [HttpPost("login")]
     public async Task<ActionResult<LoginUserDto>> Login(LoginUserDto request)
     {
-        if (_context.Users == null) return NotFound();
-
-        var user = await _context.Users.FirstOrDefaultAsync(u => u.Email == request.Email);
+        var user = await _userManagementService.AttemptLogin(request);
 
         if (user == null)
-            return Unauthorized("Wrong email");
-
-        if (!_authManager.VerifyPasswordHash(request.Password, user.Password))
-            return Unauthorized("Wrong password");
-
+            return NotFound();
+        
         var encryptedToken = _authManager.CreateAuthenticationToken(request, user.Role);
         HttpContext.Response.Cookies.Append("token", encryptedToken, new CookieOptions
         {
@@ -48,49 +37,17 @@ public class UserController : ControllerBase
             IsEssential = true,
             SameSite = SameSiteMode.None,
         });
-        return Ok();
+        return Ok(user.UserId);
     }
-
-    // PUT: api/User/5
-    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-    [HttpPut("update")]
-    [Authorize( Roles = "Admin")]
-
-    public async Task<IActionResult> Update(LoginUserDto request)
-    {
-        try
-        {
-            await _context.SaveChangesAsync();
-        }
-        catch (DbUpdateConcurrencyException)
-        {
-            if (!UserExists(request.Email))
-                return NotFound();
-            else
-                throw;
-        }
-
-        return Ok();
-    }
-
     // POST: api/User
-    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
     [HttpPost("register")]
     public async Task<ActionResult<User>> Register(UserDto request)
     {
-        if (_context.Users == null) return Problem("Entity set 'SchoolDbContext.Users'  is null.");
 
-        if (UserExists(request.Email))
-            return Conflict("User already exists");
-        var user = _userFactory.CreateUser(request);
-        _context.Users.Add(user);
-        await _context.SaveChangesAsync();
-
+        var result = await _userManagementService.CreateUser(request);
+        if (result == Status.Fail)
+            return StatusCode(502);
+        
         return CreatedAtAction("Register", "Registered user");
-    }
-
-    private bool UserExists(string email)
-    {
-        return (_context.Users?.Any(e => e.Email == email)).GetValueOrDefault();
     }
 }
