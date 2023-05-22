7a8f95414a11ea2751ed8b3c4239b85dd21efa3d
Spas Milenkov <spasmilenkov123@gmail.com>
2023-05-22 11:23:29 +0300

    Create pinia instance

--- UI/src/main.js
+++ UI/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import mitt from 'mitt'
+import { createPinia } from 'pinia'
 import './assets/main.css'
 
 import { OhVueIcon, addIcons } from 'oh-vue-icons'
@@ -32,11 +33,13 @@ addIcons(
   MdWeb,
   HiSolidChartPie
 )
+const pinia = createPinia()
 const app = createApp(App)
 const emitter = mitt()
 app.config.globalProperties.emitter = emitter
 app.provide('emitter', emitter)
 app.component('v-icon', OhVueIcon)
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
