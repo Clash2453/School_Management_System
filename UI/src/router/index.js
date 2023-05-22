f9f17d0754ee8702b39f021c5847ff3c38eb7b76
Spas Milenkov <spasmilenkov123@gmail.com>
2023-05-22 11:24:07 +0300

    Add navigation guard using pinia

--- UI/src/router/index.js
+++ UI/src/router/index.js
@@ -1,39 +1,87 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { useUserStore } from '../stores/UserStore.js'
 import Home from '../views/HomeView.vue'
 import Login from '../views/LoginView.vue'
 import Dashboard from '../views/DashboardView.vue'
-import OverviewView from '../views/OverviewView.vue'
+import OverviewView from '../views/StudentOverviewView.vue'
 import GradesView from '../views/GradesView.vue'
 import AbsenceView from '../views/AbsenceView.vue'
 import TeachersView from '../views/TeachersView.vue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', component: Home },
-    { path: `/dashboard`, component: Dashboard },
-    { path: '/login', component: Login },
     {
-      path: '/dashboard',
+      path: '/',
+      component: Home,
+      meta: {
+        requiresAuthentication: false
+      }
+    },
+    {
+      path: `/dashboard`,
       component: Dashboard,
+      meta: {
+        requiresAuthentication: false
+      }
+    },
+    {
+      path: '/login',
+      component: Login,
+      meta: {
+        requiresAuthentication: false
+      }
+    },
+    {
+      path: '/dashboard/',
+      component: Dashboard,
+      meta: {
+        requiresAuthentication: true
+      },
       children: [
         {
           path: 'overview',
-          component: OverviewView
+          component: OverviewView,
+          meta: {
+            requiresAuthentication: true
+          }
         },
         {
           path: 'grades',
-          component: GradesView
+          component: GradesView,
+          meta: {
+            requiresAuthentication: true
+          }
         },
         {
           path: 'absence',
-          component: AbsenceView
+          component: AbsenceView,
+          meta: {
+            requiresAuthentication: true
+          }
         },
         {
           path: 'teachers',
-          component: TeachersView
+          component: TeachersView,
+          meta: {
+            requiresAuthentication: true
+          }
         }
       ]
     }
   ]
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((page) => page.meta.requiresAuthentication)) {
+    const store = useUserStore()
+
+    if (!store.authenticationStatus)
+      next({
+        path: '/login',
+        component: Login
+      })
+    else next()
+  } else next()
 })
 export default router
