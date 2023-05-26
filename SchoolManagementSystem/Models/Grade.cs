7434e69ef53f7a6475cb1caaaf2ea44bd72902c9
Spas Milenkov <spasmilenkov123@gmail.com>
2023-05-26 19:09:35 +0300

    Add subject relation

--- SchoolManagementSystem/Models/Grade.cs
+++ SchoolManagementSystem/Models/Grade.cs
@@ -2,9 +2,10 @@ namespace SchoolManagementSystem.Models;
 
 public class Grade
 {
-    public float Value { get; set; }
     public int GradeId { get; set; }
+    public float Value { get; set; }
     public DateTime Date { get; set; }
     public virtual Student Owner { get; set; } = null!;
     public virtual Teacher Grader { get; set; } = null!;
+    public virtual Subject Subject { get; set; } = null!;
 }
