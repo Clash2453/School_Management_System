b4d3b78fbbf3495da6c67b18d36ae3d7b952e867
Spas Milenkov <spasmilenkov123@gmail.com>
2023-05-28 19:47:44 +0300

    Add background to the DashboardView.vue

--- UI/src/views/DashboardView.vue
+++ UI/src/views/DashboardView.vue
@@ -10,6 +10,9 @@ import Sidebar from '../components/userDashboard/SidebarComponent.vue'
 <style>
 .flex {
   flex: 1;
+  /* background-color: #0b2239; */
+  background: url('../public/images/dashboard-background.svg') no-repeat;
+  background-size: cover;
 }
 #table-section {
   display: flex;
