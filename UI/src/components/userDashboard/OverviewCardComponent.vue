91d6fdf4eabc2cb276a81a52652dd8acecb56a59
Spas Milenkov <spasmilenkov123@gmail.com>
2023-05-28 19:45:02 +0300

    Add props for dynamically fetching the data in the card

--- UI/src/components/userDashboard/OverviewCardComponent.vue
+++ UI/src/components/userDashboard/OverviewCardComponent.vue
@@ -1,13 +1,30 @@
 <script setup>
 import RoundLoader from './RoundLoaderComponent.vue'
+const props = defineProps([
+  'mainContent',
+  'firstArgument',
+  'secondArgument',
+  'backColor',
+  'displayedValue',
+  'loaderNeeded',
+  'cardTitle'
+])
+const startColor = '#ceff8f'
+const endColor = '#0eff00'
 
-const props = defineProps(['mainContent', 'firstArgument', 'secondArgument'])
+console.log(props.mainContent)
 </script>
 <template>
   <section class="overview">
-    <RoundLoader></RoundLoader>
+    <RoundLoader
+      v-if="props.loaderNeeded"
+      :starting-color="startColor"
+      :ending-color="endColor"
+      :value="parseFloat(props.displayedValue)"
+    ></RoundLoader>
     <div class="stats-container">
-      <h3 class="average subtitle">{{ mainContent }}</h3>
+      <h2 class="subtitle card-title">{{ props.cardTitle }}</h2>
+      <h3 class="average subtitle">{{ props.mainContent }}</h3>
       <h4 class="discipline">{{ firstArgument }}</h4>
       <h4 class="discipline">{{ secondArgument }}</h4>
     </div>
@@ -16,29 +33,51 @@ const props = defineProps(['mainContent', 'firstArgument', 'secondArgument'])
 <style scoped>
 .overview {
   display: flex;
-  align-items: center;
   justify-content: center;
+  align-items: center;
   padding: 1rem;
   gap: 1rem;
-  background-color: #2a619b;
+  background-color: v-bind('$props.backColor');
   border-radius: 5px;
   color: white;
   box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
-  width: 30rem;
+  width: clamp(20rem, 30%, 40rem);
   height: 100%;
+  min-height: 14.5rem;
 }
 .stats-container {
   width: 100%;
   height: 100%;
-  padding: 1rem;
+  padding: 0.5rem;
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
   align-items: flex-start;
   gap: 0.5rem;
 }
+.card-title {
+  font-size: 1.5rem;
+  margin-bottom: 1rem;
+  font-weight: 900;
+}
 .discipline {
   font-family: 'Lato', sans-serif;
   font-size: 1.125rem;
+}
+@media (max-width: 1350px) {
+  .overview {
+    /* flex-direction: column; */
+    width: 30rem;
+  }
+  .discipline {
+    font-size: 1rem;
+  }
+}
+@media (max-width: 550px) {
+  .overview {
+    flex-direction: column;
+    width: fit-content;
+    padding: 1rem;
+  }
 }
 </style>
