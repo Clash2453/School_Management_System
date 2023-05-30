4f239bfbdd4d2bb4272635811c71c8acd3e2f0c1
Spas Milenkov <spasmilenkov123@gmail.com>
2023-05-30 19:58:14 +0300

    Update the data bundling interface

--- SchoolManagementSystem/Interfaces/IDataBundlingService.cs
+++ SchoolManagementSystem/Interfaces/IDataBundlingService.cs
@@ -7,4 +7,5 @@ public interface IDataBundlingService
     public Task<StudentDataDto?> OrganizeStudentData(int id);
     public Task<GradeDataDto?> OrganizeStudentGradeData(int id);
     public Task<GradeDataDto?> OrganizeTeacherGradeData(int id);
+    public Task<Dictionary<string, List<GradeResultDto>>?> OrganizeGradesPerSubjects(int id);
 }
