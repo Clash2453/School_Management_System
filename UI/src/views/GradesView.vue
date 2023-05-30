dc1f443f69f60bdc4feda8ea33267244ec8e8c0c
Spas Milenkov <spasmilenkov123@gmail.com>
2023-05-30 19:57:25 +0300

    Add the the grades per subject grid

--- UI/src/views/GradesView.vue
+++ UI/src/views/GradesView.vue
@@ -1,5 +1,49 @@
-<script></script>
+<script setup>
+import axios from 'axios'
+import DataGrid from '../components/userDashboard/DataGridComponent.vue'
+import { onMounted, ref } from 'vue'
+let grades = ref({})
+let dataFetched = ref(false)
+onMounted(async () => {
+  const data = await getGradesPerSubject()
+  console.log(data)
+  grades.value = {
+    headers: ['Subjects', 'First Term', 'Term Grade', 'Second Term', 'Term Grade', 'Year Grade'],
+    subjects: Object.keys(data),
+    grades: data
+  }
+  console.log(grades.value)
+  dataFetched.value = true
+})
+async function getGradesPerSubject() {
+  try {
+    const response = await axios({
+      method: 'GET',
+      url: `https://localhost:7080/api/Grade/subjects-with-grades`,
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      withCredentials: true
+    })
+    console.log(response.data)
+    return response.data
+  } catch (e) {
+    console.log(e)
+  }
+}
+</script>
 <template>
-  <h1>Hi this is grades</h1>
+  <section id="grade-section">
+    <DataGrid :options="grades" v-if="dataFetched"></DataGrid>
+  </section>
 </template>
-<style></style>
+<style scoped>
+#grade-section {
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
