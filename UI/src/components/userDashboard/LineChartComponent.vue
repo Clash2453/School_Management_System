98a9385db00db25dc3f7057efb07b30dc978c884
Spas Milenkov <spasmilenkov123@gmail.com>
2023-05-31 21:37:24 +0300

    Add line chart component

---
+++ UI/src/components/userDashboard/LineChartComponent.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="chart">
+    <Line :options="chartOptions" :data="props.chartData" />
+  </div>
+</template>
+
+<script setup>
+import { Line } from 'vue-chartjs'
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  PointElement,
+  LineElement,
+  CategoryScale,
+  LinearScale
+} from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, PointElement, LineElement, CategoryScale, LinearScale)
+
+const props = defineProps(['chartData'])
+const chartOptions = {
+  responsive: true,
+  plugins: {
+    legend: {
+      labels: {
+        color: 'rgb(255, 255, 255)'
+      }
+    }
+  },
+  scales: {
+    y: {
+      ticks: { color: 'rgba(255, 255, 255, 1)' },
+      suggestedMin: 2,
+      suggestedMax: 6,
+      grid: {
+        color: 'rgba(255, 255, 255, 0.7)'
+      }
+    },
+    x: {
+      ticks: { color: 'rgba(255, 255, 255, 1)' },
+      grid: {
+        color: 'rgba(255, 255, 255, 0.7)'
+      }
+    }
+  }
+}
+</script>
+<style scoped>
+.chart {
+  width: 31rem;
+  height: 20rem;
+  background-color: #2a619b;
+  border-radius: 5px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 1rem;
+}
+</style>
