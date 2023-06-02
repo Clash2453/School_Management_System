5b1addbdc69e144af9027127d3c0c3f6fc365aba
Spas Milenkov <spasmilenkov123@gmail.com>
2023-06-02 21:42:24 +0300

    Add method for the admin to get guest users

--- SchoolManagementSystem/Controllers/AdminController.cs
+++ SchoolManagementSystem/Controllers/AdminController.cs
@@ -8,49 +8,60 @@ namespace SchoolManagementSystem.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-[Authorize( Roles = "Admin")]
+[Authorize(Roles = "Admin")]
 public class AdminController : ControllerBase
 {
-    private readonly IUserManagementService _specializationService;
-    public AdminController(IUserManagementService specializationService)
+    private readonly IUserManagementService _userManagementService;
+
+    public AdminController(IUserManagementService userManagementService)
     {
-        _specializationService = specializationService;
+        _userManagementService = userManagementService;
     }
-        
+    
+    [Authorize(Roles = "Admin")]
+    [HttpGet("guests")]
+    public async Task<IActionResult> FetchGuests()  
+    {
+        return Ok(await _userManagementService.FetchGuests());
+    }
+
     [HttpPost("approve/student")]
     public async Task<IActionResult> AssignStudent(StudentDto request)
     {
-        var status = await _specializationService.CreateStudent(request);
+        var status = await _userManagementService.CreateStudent(request);
         if (status == Status.Fail)
             return StatusCode(500);
-            
+
         return Ok("Student added successfully");
     }
+
     [HttpPost("approve/teacher")]
-    public async Task<IActionResult> AssignTeacher( TeacherDto request)
+    public async Task<IActionResult> AssignTeacher(TeacherDto request)
     {
-        var status = await _specializationService.CreateTeacher(request);
+        var status = await _userManagementService.CreateTeacher(request);
         if (status == Status.Fail)
             return StatusCode(500);
-            
+
         return Ok("Teacher added successfully");
     }
+
     [HttpPost("approve/admin")]
     public async Task<IActionResult> AssignAdmin(AdminDto request)
     {
-        var status = await _specializationService.CreateAdmin(request);
+        var status = await _userManagementService.CreateAdmin(request);
         if (status == Status.Fail)
             return StatusCode(500);
-            
+
         return Ok("Admin added successfully");
     }
+ 
     [HttpDelete("admin/delete-user")]
     public async Task<IActionResult> DeleteUser(int id)
     {
-        var status = await _specializationService.DeleteUser(id);
+        var status = await _userManagementService.DeleteUser(id);
         if (status == Status.Fail)
             return StatusCode(500);
-            
+
         return Ok("Admin added successfully");
     }
 }
