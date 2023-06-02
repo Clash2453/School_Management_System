2e540b3a586b3cdcef70cbde0a2ab7186c10e662
Spas Milenkov <spasmilenkov123@gmail.com>
2023-06-02 21:43:57 +0300

    Require the grade type in the grade dto

--- SchoolManagementSystem/Models/DataTransferObjects/GradeDto.cs
+++ SchoolManagementSystem/Models/DataTransferObjects/GradeDto.cs
@@ -1,10 +1,13 @@
+using SchoolManagementSystem.Enums;
+
 namespace SchoolManagementSystem.Models.DataTransferObjects;
 
 public class GradeDto
 {
     public float Value { get; set; }
-    public DateTime Date = DateTime.Now;
+    public DateTime Date = DateTime.Now;    
     public int StudentId { get; set; }
     public int TeacherId { get; set; }
     public int SubjectId { get; set; }
+    public GradeType GradeType { get; set; }
 }
