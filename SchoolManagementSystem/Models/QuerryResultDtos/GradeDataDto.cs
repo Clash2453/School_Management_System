8e5fcce67b81f17f9f66ac9e31f8cdc360338f51
Spas Milenkov <spasmilenkov123@gmail.com>
2023-06-02 21:43:46 +0300

    Add lists for the different grade types

--- SchoolManagementSystem/Models/QuerryResultDtos/GradeDataDto.cs
+++ SchoolManagementSystem/Models/QuerryResultDtos/GradeDataDto.cs
@@ -3,5 +3,7 @@ namespace SchoolManagementSystem.Models.QuerryResultDtos;
 public class GradeDataDto
 {
     public List<List<GradeResultDto>> Grades { get; set; } = new();
+    public List<List<GradeResultDto>> TermGrades { get; set; } = new();
+    public List<List<GradeResultDto>> YearlyGrades { get; set; } = new();
 
 }
