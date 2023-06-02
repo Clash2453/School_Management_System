72b1e624ce3d0368904855c4bd3060673f27a50d
Spas Milenkov <spasmilenkov123@gmail.com>
2023-06-02 21:43:32 +0300

    Add discrimantor column for the different grade types

--- SchoolManagementSystem/Models/Grade.cs
+++ SchoolManagementSystem/Models/Grade.cs
@@ -1,3 +1,5 @@
+using SchoolManagementSystem.Enums;
+
 namespace SchoolManagementSystem.Models;
 
 public class Grade
@@ -5,6 +7,9 @@ public class Grade
     public int GradeId { get; set; }
     public float Value { get; set; }
     public DateTime Date { get; set; }
+    public GradeType  TypeOfGrade { get; set; }
+    public int Term { get; set; }
+    public int StudentYear { get; set; }
     public virtual Student Owner { get; set; } = null!;
     public virtual Teacher Grader { get; set; } = null!;
     public virtual Subject Subject { get; set; } = null!;
