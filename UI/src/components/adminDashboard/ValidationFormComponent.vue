69428af3d548730066c42d6aaad4df4bc7706a97
Spas Milenkov <spasmilenkov123@gmail.com>
2023-06-03 20:23:13 +0300

    Add the validation form component

---
+++ UI/src/components/adminDashboard/ValidationFormComponent.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { ref } from 'vue'
+const isStudent = ref('true')
+const isTeacher = ref('false')
+</script>
+<template>
+  <div>
+    <form action="" id="studentForm" v-if="isTeacher">
+      <div class="input-wrapper">
+        <label></label>
+        <input type="text" name="" id="" />
+      </div>
+    </form>
+    <form action="" id="student-form" v-if="isStudent">
+      <div class="input-wrapper">
+        <label for=""></label>
+        <input type="text" name="" id="" />
+      </div>
+      <div class="input-wrapper">
+        <label for=""></label>
+        <input type="text" name="" id="" />
+      </div>
+      <div class="input-wrapper">
+        <label for=""></label>
+        <input type="text" name="" id="" />
+      </div>
+      <div class="input-wrapper">
+        <label for=""></label>
+        <input type="text" name="" id="" />
+      </div>
+      <button class="submit-button"></button>
+    </form>
+  </div>
+</template>
+<style scoped></style>
