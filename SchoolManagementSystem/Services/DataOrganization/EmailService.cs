ac145ea3b957018d7afbaca6480b6b02221335dd
Spas Milenkov <spasmilenkov123@gmail.com>
2023-06-03 20:21:23 +0300

    Add email sending service

---
+++ SchoolManagementSystem/Services/DataOrganization/EmailService.cs
@@ -0,0 +1,36 @@
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+using MimeKit.Text;
+using SchoolManagementSystem.Enums;
+using SchoolManagementSystem.Interfaces;
+using SchoolManagementSystem.Models.DataTransferObjects;
+
+namespace SchoolManagementSystem.Services.DataOrganization;
+
+public class EmailService : IEmailService
+{
+    private readonly IConfiguration _configuration;
+
+    public EmailService(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+    public Status SendMail(EmailDto request)
+    { 
+        var email = new MimeMessage();
+        email.From.Add(MailboxAddress.Parse(_configuration.GetSection("EmailHost").Value));
+        email.To.Add(MailboxAddress.Parse(request.Receiver));
+        email.Subject = request.Subject;
+        var bodyBuilder = new BodyBuilder();
+        bodyBuilder.HtmlBody = request.Body;
+        email.Body = bodyBuilder.ToMessageBody();
+
+        using var smtp = new SmtpClient();
+        smtp.Connect(_configuration.GetSection("EmailHost").Value, 587, SecureSocketOptions.StartTls);
+        smtp.Authenticate(_configuration.GetSection("EmailAddress").Value, _configuration.GetSection("EmailPassword").Value);
+        smtp.Send(email);
+        smtp.Disconnect(true);
+        return Status.Success; 
+    }
+}
