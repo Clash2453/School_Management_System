c063fecfed73a8feeba9eee0994abc4ccaa32f54
Spas Milenkov <spasmilenkov123@gmail.com>
2023-06-04 17:47:19 +0300

    Add Migrations

---
+++ SchoolManagementSystem/Migrations/20230519145355_ChangeSubjects.cs
@@ -0,0 +1,439 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SchoolManagementSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeSubjects : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Students_Subjects_SubjectId",
+                table: "Students");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Students_Subjects_SubjectId1",
+                table: "Students");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_StudentSubjects_Subjects_SubjectId",
+                table: "StudentSubjects");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_TeacherSubjects_Subjects_SubjectId",
+                table: "TeacherSubjects");
+
+            migrationBuilder.DropTable(
+                name: "StudentEvents");
+
+            migrationBuilder.DropTable(
+                name: "TeacherEvents");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_TeacherSubjects",
+                table: "TeacherSubjects");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_StudentSubjects",
+                table: "StudentSubjects");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Students_SubjectId",
+                table: "Students");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Students_SubjectId1",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "Subject",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "SubjectId",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "SubjectId1",
+                table: "Students");
+
+            migrationBuilder.RenameColumn(
+                name: "SubjectId",
+                table: "Subjects",
+                newName: "Id");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SubjectId",
+                table: "TeacherSubjects",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "TeacherSubjects",
+                type: "int",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SubjectId",
+                table: "StudentSubjects",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "StudentSubjects",
+                type: "int",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn);
+
+            migrationBuilder.AddColumn<int>(
+                name: "StudentId",
+                table: "Events",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TeacherId",
+                table: "Events",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Date",
+                table: "Absences",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Excused",
+                table: "Absences",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "SubjectId",
+                table: "Absences",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_TeacherSubjects",
+                table: "TeacherSubjects",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_StudentSubjects",
+                table: "StudentSubjects",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TeacherSubjects_SubjectId",
+                table: "TeacherSubjects",
+                column: "SubjectId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StudentSubjects_SubjectId",
+                table: "StudentSubjects",
+                column: "SubjectId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Events_StudentId",
+                table: "Events",
+                column: "StudentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Events_TeacherId",
+                table: "Events",
+                column: "TeacherId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Absences_SubjectId",
+                table: "Absences",
+                column: "SubjectId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Absences_Subjects_SubjectId",
+                table: "Absences",
+                column: "SubjectId",
+                principalTable: "Subjects",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Events_Students_StudentId",
+                table: "Events",
+                column: "StudentId",
+                principalTable: "Students",
+                principalColumn: "StudentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Events_Teacher_TeacherId",
+                table: "Events",
+                column: "TeacherId",
+                principalTable: "Teacher",
+                principalColumn: "TeacherId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_StudentSubjects_Subjects_SubjectId",
+                table: "StudentSubjects",
+                column: "SubjectId",
+                principalTable: "Subjects",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TeacherSubjects_Subjects_SubjectId",
+                table: "TeacherSubjects",
+                column: "SubjectId",
+                principalTable: "Subjects",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Absences_Subjects_SubjectId",
+                table: "Absences");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Events_Students_StudentId",
+                table: "Events");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Events_Teacher_TeacherId",
+                table: "Events");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_StudentSubjects_Subjects_SubjectId",
+                table: "StudentSubjects");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_TeacherSubjects_Subjects_SubjectId",
+                table: "TeacherSubjects");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_TeacherSubjects",
+                table: "TeacherSubjects");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TeacherSubjects_SubjectId",
+                table: "TeacherSubjects");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_StudentSubjects",
+                table: "StudentSubjects");
+
+            migrationBuilder.DropIndex(
+                name: "IX_StudentSubjects_SubjectId",
+                table: "StudentSubjects");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Events_StudentId",
+                table: "Events");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Events_TeacherId",
+                table: "Events");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Absences_SubjectId",
+                table: "Absences");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "TeacherSubjects");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "StudentSubjects");
+
+            migrationBuilder.DropColumn(
+                name: "StudentId",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "TeacherId",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "Date",
+                table: "Absences");
+
+            migrationBuilder.DropColumn(
+                name: "Excused",
+                table: "Absences");
+
+            migrationBuilder.DropColumn(
+                name: "SubjectId",
+                table: "Absences");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Subjects",
+                newName: "SubjectId");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SubjectId",
+                table: "TeacherSubjects",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SubjectId",
+                table: "StudentSubjects",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Subject",
+                table: "Students",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<int>(
+                name: "SubjectId",
+                table: "Students",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "SubjectId1",
+                table: "Students",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_TeacherSubjects",
+                table: "TeacherSubjects",
+                column: "SubjectId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_StudentSubjects",
+                table: "StudentSubjects",
+                column: "SubjectId");
+
+            migrationBuilder.CreateTable(
+                name: "StudentEvents",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    StudentId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StudentEvents", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_StudentEvents_Events_Id",
+                        column: x => x.Id,
+                        principalTable: "Events",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_StudentEvents_Students_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Students",
+                        principalColumn: "StudentId");
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "TeacherEvents",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    TeacherId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TeacherEvents", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TeacherEvents_Events_Id",
+                        column: x => x.Id,
+                        principalTable: "Events",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TeacherEvents_Teacher_TeacherId",
+                        column: x => x.TeacherId,
+                        principalTable: "Teacher",
+                        principalColumn: "TeacherId");
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_SubjectId",
+                table: "Students",
+                column: "SubjectId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_SubjectId1",
+                table: "Students",
+                column: "SubjectId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StudentEvents_StudentId",
+                table: "StudentEvents",
+                column: "StudentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TeacherEvents_TeacherId",
+                table: "TeacherEvents",
+                column: "TeacherId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Students_Subjects_SubjectId",
+                table: "Students",
+                column: "SubjectId",
+                principalTable: "Subjects",
+                principalColumn: "SubjectId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Students_Subjects_SubjectId1",
+                table: "Students",
+                column: "SubjectId1",
+                principalTable: "Subjects",
+                principalColumn: "SubjectId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_StudentSubjects_Subjects_SubjectId",
+                table: "StudentSubjects",
+                column: "SubjectId",
+                principalTable: "Subjects",
+                principalColumn: "SubjectId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TeacherSubjects_Subjects_SubjectId",
+                table: "TeacherSubjects",
+                column: "SubjectId",
+                principalTable: "Subjects",
+                principalColumn: "SubjectId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ SchoolManagementSystem/Migrations/20230520122209_EventAndSubjectJoinTables.cs
@@ -0,0 +1,263 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SchoolManagementSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class EventAndSubjectJoinTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_StudentSubjects_Students_StudentId",
+                table: "StudentSubjects");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_StudentSubjects_Subjects_SubjectId",
+                table: "StudentSubjects");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_TeacherSubjects_Subjects_SubjectId",
+                table: "TeacherSubjects");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_TeacherSubjects_Teacher_TeacherId",
+                table: "TeacherSubjects");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TeacherId",
+                table: "TeacherSubjects",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SubjectId",
+                table: "TeacherSubjects",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SubjectId",
+                table: "StudentSubjects",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "StudentId",
+                table: "StudentSubjects",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "StudentEvents",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    StudentId = table.Column<int>(type: "int", nullable: false),
+                    EventId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StudentEvents", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_StudentEvents_Events_EventId",
+                        column: x => x.EventId,
+                        principalTable: "Events",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_StudentEvents_Students_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Students",
+                        principalColumn: "StudentId",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "TeacherEvents",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    TeacherId = table.Column<int>(type: "int", nullable: false),
+                    EventId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TeacherEvents", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TeacherEvents_Events_EventId",
+                        column: x => x.EventId,
+                        principalTable: "Events",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TeacherEvents_Teacher_TeacherId",
+                        column: x => x.TeacherId,
+                        principalTable: "Teacher",
+                        principalColumn: "TeacherId",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StudentEvents_EventId",
+                table: "StudentEvents",
+                column: "EventId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StudentEvents_StudentId",
+                table: "StudentEvents",
+                column: "StudentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TeacherEvents_EventId",
+                table: "TeacherEvents",
+                column: "EventId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TeacherEvents_TeacherId",
+                table: "TeacherEvents",
+                column: "TeacherId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_StudentSubjects_Students_StudentId",
+                table: "StudentSubjects",
+                column: "StudentId",
+                principalTable: "Students",
+                principalColumn: "StudentId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_StudentSubjects_Subjects_SubjectId",
+                table: "StudentSubjects",
+                column: "SubjectId",
+                principalTable: "Subjects",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TeacherSubjects_Subjects_SubjectId",
+                table: "TeacherSubjects",
+                column: "SubjectId",
+                principalTable: "Subjects",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TeacherSubjects_Teacher_TeacherId",
+                table: "TeacherSubjects",
+                column: "TeacherId",
+                principalTable: "Teacher",
+                principalColumn: "TeacherId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_StudentSubjects_Students_StudentId",
+                table: "StudentSubjects");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_StudentSubjects_Subjects_SubjectId",
+                table: "StudentSubjects");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_TeacherSubjects_Subjects_SubjectId",
+                table: "TeacherSubjects");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_TeacherSubjects_Teacher_TeacherId",
+                table: "TeacherSubjects");
+
+            migrationBuilder.DropTable(
+                name: "StudentEvents");
+
+            migrationBuilder.DropTable(
+                name: "TeacherEvents");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TeacherId",
+                table: "TeacherSubjects",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SubjectId",
+                table: "TeacherSubjects",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SubjectId",
+                table: "StudentSubjects",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "StudentId",
+                table: "StudentSubjects",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_StudentSubjects_Students_StudentId",
+                table: "StudentSubjects",
+                column: "StudentId",
+                principalTable: "Students",
+                principalColumn: "StudentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_StudentSubjects_Subjects_SubjectId",
+                table: "StudentSubjects",
+                column: "SubjectId",
+                principalTable: "Subjects",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TeacherSubjects_Subjects_SubjectId",
+                table: "TeacherSubjects",
+                column: "SubjectId",
+                principalTable: "Subjects",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TeacherSubjects_Teacher_TeacherId",
+                table: "TeacherSubjects",
+                column: "TeacherId",
+                principalTable: "Teacher",
+                principalColumn: "TeacherId");
+        }
+    }
+}
