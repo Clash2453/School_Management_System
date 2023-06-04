b49de7fa64cce30a49cdd472d2902881d6b3cdac
Spas Milenkov <spasmilenkov123@gmail.com>
2023-06-04 20:31:37 +0300

    Add endpoints for fetching faculties and specialties

--- SchoolManagementSystem/Models/DataTransferObjects/SubjectDto.cs
+++ SchoolManagementSystem/Models/DataTransferObjects/SubjectDto.cs
@@ -3,6 +3,5 @@ namespace SchoolManagementSystem.Models.DataTransferObjects;
 public class SubjectDto
 {
     public string Name { get; set; } = null!;
-    public List<int> StudentIds { get; set; } = null!;
     public List<int> TeacherIds { get; set; } = null!;
 }
