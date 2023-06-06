e01bdc7b584f9146b083eb9d13bfdb8492b56080
Spas Milenkov <spasmilenkov123@gmail.com>
2023-06-06 21:46:54 +0300

    Add id prop

--- SchoolManagementSystem/Models/QuerryResultDtos/SpecialtyQueryResult.cs
+++ SchoolManagementSystem/Models/QuerryResultDtos/SpecialtyQueryResult.cs
@@ -2,6 +2,7 @@ namespace SchoolManagementSystem.Models.QuerryResultDtos;
 
 public class SpecialtyQueryResult
 {
+    public int Id { get; set; }
     public string Name { get; set; } = null!;
     public List<string> Subjects { get; set; } = null!;
 }
