b6d14e7b517519f864cdb16e339050364a4c24bf
Spas Milenkov <spasmilenkov123@gmail.com>
2023-06-06 21:46:38 +0300

    Update the subject class for the new db structure

--- SchoolManagementSystem/Models/Subject.cs
+++ SchoolManagementSystem/Models/Subject.cs
@@ -8,5 +8,6 @@ public class Subject
     [Key]
     public int Id { get; set; }
     public string Name { get; set; } = null!;
-    public virtual List<TeacherSubject> Teacher { get; set; } = null!;
-}
+    public virtual List<TeacherSubject> Teachers { get; set; } = null!;
+    public List<SubjectSpecialty> SubjectSpecialties { get; set; } = null!; 
+}   
