4c24dd133ccea0c0abf90b85780f9eca3bc16d0a
Spas Milenkov <spasmilenkov123@gmail.com>
2023-06-06 21:47:13 +0300

    Add faculty id property

--- SchoolManagementSystem/Models/QuerryResultDtos/FacultyQueryResult.cs
+++ SchoolManagementSystem/Models/QuerryResultDtos/FacultyQueryResult.cs
@@ -2,6 +2,7 @@ namespace SchoolManagementSystem.Models.QuerryResultDtos;
 
 public class FacultyQueryResult
 {
+    public int Id { get; set; }
     public string Name { get; set; }
-    public List<SpecialtyQueryResult> Specialties { get; set; }
+    public List<Specialty> Specialties { get; set; }
 }
