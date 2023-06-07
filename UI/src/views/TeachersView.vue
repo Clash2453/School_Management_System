199c31cfdc22df0c43f6347eae330a6c83b5be3e
Spas Milenkov <spasmilenkov123@gmail.com>
2023-06-07 19:05:00 +0300

    Update the teachers view with new components

--- UI/src/views/TeachersView.vue
+++ UI/src/views/TeachersView.vue
@@ -1,5 +1,52 @@
-<script></script>
+<script setup>
+import { onMounted, ref } from 'vue'
+import OverviewCardComponent from '../components/userDashboard/OverviewCardComponent.vue'
+import StudentList from '../components/teacherDashboard/StudentListComponent.vue'
+import axios from 'axios'
+const options = ref({
+  mainContent: `Your teacher ID is: `,
+  firstArgument: ``,
+  secondArgument: ``,
+  displayedValue: 5,
+  backColor: '#33b864',
+  loaderNeeded: false,
+  cardTitle: `Welcome `
+})
+onMounted(async () => {
+  const teacherData = await fetchTeacherData()
+  options.value.mainContent = `Your teacher ID is: ${teacherData.id}`
+  options.value.cardTitle = `Welcome ${teacherData.name}`
+})
+async function fetchTeacherData() {
+  try {
+    const result = await axios({
+      method: 'GET',
+      url: `https://localhost:7080/teachers`,
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      withCredentials: true
+    })
+    console.log(result.data)
+    return result.data
+  } catch (e) {
+    console.log(e)
+  }
+}
+</script>
 <template>
-  <h1>Hi this is teachers view</h1>
+  <section class="section-container">
+    <OverviewCardComponent :card-data="options" />
+    <StudentList />
+  </section>
 </template>
-<style></style>
+<style scoped>
+.section-container {
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  padding: 1rem;
+  gap: 1rem;
+}
+</style>
