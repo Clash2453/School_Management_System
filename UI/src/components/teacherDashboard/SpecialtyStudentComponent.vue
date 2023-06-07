3dd99dd1bcc4df5085f27adc9c9f318b9cbc5905
Spas Milenkov <spasmilenkov123@gmail.com>
2023-06-07 19:06:00 +0300

    Add student list components for presenting students in the teachers view

---
+++ UI/src/components/teacherDashboard/SpecialtyStudentComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div
+    class="container"
+    v-for="(specialty, index) in specialtyWithStudents.specialties"
+    :key="index"
+  >
+    <h3 class="subtitle">specialty.name</h3>
+    <ul>
+      <li v-for="(student, i) in specialtyWithStudents[specialty]" :key="i">{{ student.name }}</li>
+    </ul>
+  </div>
+</template>
+<script setup>
+const props = defineProps(['specialtyWithStudents'])
+</script>
+<style scoped></style>
---
+++ UI/src/components/teacherDashboard/StudentListComponent.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="list-container">
+    <h1 class="main-title align-c">Applications waiting approval</h1>
+    <ul class="list">
+      <li class="list-item" v-for="(guest, index) in guests" :key="index"></li>
+    </ul>
+  </div>
+</template>
+<script setup></script>
+<style scoped>
+/* .list-container; */
+</style>
