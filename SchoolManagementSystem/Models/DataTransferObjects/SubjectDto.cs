e03fd74596a6d91a942b53845b0f63a855a68ce6
Spas Milenkov <spasmilenkov123@gmail.com>
2023-06-07 19:07:12 +0300

    Add id

--- SchoolManagementSystem/Models/DataTransferObjects/SubjectDto.cs
+++ SchoolManagementSystem/Models/DataTransferObjects/SubjectDto.cs
@@ -2,6 +2,9 @@ namespace SchoolManagementSystem.Models.DataTransferObjects;
 
 public class SubjectDto
 {
+    public int Id { get; set; }
     public string Name { get; set; } = null!;
     public List<int> TeacherIds { get; set; } = null!;
+    
+
 }
