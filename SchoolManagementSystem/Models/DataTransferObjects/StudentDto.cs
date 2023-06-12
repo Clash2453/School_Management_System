1be27e97317d5c7800364c3f4a294b123cafd30a
Spas Milenkov <spasmilenkov123@gmail.com>
2023-06-12 22:53:36 +0300

    Add email property

--- SchoolManagementSystem/Models/DataTransferObjects/StudentDto.cs
+++ SchoolManagementSystem/Models/DataTransferObjects/StudentDto.cs
@@ -2,7 +2,8 @@ namespace SchoolManagementSystem.Models.DataTransferObjects;
 
 public class StudentDto
 {
-    public int UserId { get; set; } 
+    public int UserId { get; set; }
+    public string Email { get; set; } = null!;
     public int Specialty { get; set; }
     public int Faculty { get; set; }
     public int Group { get; set; }  
