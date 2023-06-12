1d22314d16d5f342d8cc5ea843a913ca4a403ea0
Spas Milenkov <spasmilenkov123@gmail.com>
2023-06-12 22:52:22 +0300

    Add large list for the different forms

---
+++ UI/src/components/userDashboard/LargeListComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <ul class="list">
+    <LargeListItemComponent :options="data" />
+    <LargeListItemComponent :options="data" />
+    <LargeListItemComponent :options="data" />
+    <LargeListItemComponent :options="data" />
+    <LargeListItemComponent :options="data" />
+  </ul>
+</template>
+<script setup>
+import LargeListItemComponent from './LargeListItemComponent.vue'
+const data = {
+  firstContent: 'FirstContent',
+  firstSubContent: 'firstSubContent',
+  secondContent: 'secondContent',
+  secondSubContent: 'secondSubContent'
+}
+</script>
+<style scoped>
+
+</style>
---
+++ UI/src/components/userDashboard/LargeListItemComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <li class="list-item">
+    <div class="wrapper">
+      <h2 class="subtitle">{{ options.firstContent }}</h2>
+      <p class="text">{{ options.firstSubContent }}</p>
+    </div>
+    <div class="wrapper">
+      <h2 class="subtitle">{{ options.secondContent }}</h2>
+      <p class="text">{{ options.secondSubContent }}</p>
+    </div>
+    <div class="wrapper">
+      <button class="expand-button">Expand</button>
+    </div>
+  </li>
+</template>
+<script setup>
+const props = defineProps(['options'])
+</script>
+<style scoped></style>
