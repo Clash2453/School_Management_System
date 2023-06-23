dfd0a444065a8eef1126b3e27a54b25c87588bfe
Spas Milenkov <spasmilenkov123@gmail.com>
2023-06-23 20:00:21 +0300

    Add doughnut chart base component

---
+++ UI/src/components/userDashboard/DoughnutDiagramComponent.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="chart shadow">
+    <Doughnut :data="data"></Doughnut>
+  </div>
+</template>
+<script setup>
+import { Doughnut } from 'vue-chartjs'
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  PointElement,
+  ArcElement,
+  CategoryScale,
+  LinearScale
+} from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, PointElement, ArcElement, CategoryScale, LinearScale)
+const data = {
+  labels: ['Red', 'Blue', 'Yellow'],
+  datasets: [
+    {
+      label: 'My First Dataset',
+      data: [300, 50, 100],
+      backgroundColor: ['rgb(255, 99, 132)', 'rgb(54, 162, 235)', 'rgb(255, 205, 86)'],
+      hoverOffset: 4
+    }
+  ]
+}
+</script>
+<style scoped></style>
