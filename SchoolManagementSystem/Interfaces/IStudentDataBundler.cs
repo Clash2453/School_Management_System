8252b200e908781e1d658dc29f18272c2ebacae8
Spas Milenkov <spasmilenkov123@gmail.com>
2023-07-15 16:16:50 +0300

    Add helper functions to reuse logic

--- SchoolManagementSystem/Interfaces/IStudentDataBundler.cs
+++ SchoolManagementSystem/Interfaces/IStudentDataBundler.cs
@@ -1,4 +1,6 @@
+using SchoolManagementSystem.Models;
 using SchoolManagementSystem.Models.QuerryResultDtos;
+using SchoolManagementSystem.Services.DataOrganization;
 
 namespace SchoolManagementSystem.Interfaces;
 
@@ -6,4 +8,9 @@ public interface IStudentDataBundler
 {
     public Task<StudentDataDto?> OrganizeStudentData(int id);
     public Task<GradeDataDto?> OrganizeStudentGradeData(int id);
+    public float CalculateAverageGrade(IEnumerable<GradeValue> grades);
+     
+    public List<GradeValue> GetGradeValues(List<Grade> grades);
+     
+    public Task<List<Grade>> GetStudentGrades(int studentId);
 }
