aabf340fad386e915b4bffd1137f7a06d3038875
Spas Milenkov <spasmilenkov123@gmail.com>
2023-07-15 16:18:08 +0300

    Add student gpi dto for transferring the gpi value to the client

---
+++ SchoolManagementSystem/Models/QuerryResultDtos/StudentGpiDto.cs
@@ -0,0 +1,12 @@
+namespace SchoolManagementSystem.Models.QuerryResultDtos;
+
+public class StudentGpiDto
+{
+    public KeyValuePair<string, int> AcademicPerformance;
+    public KeyValuePair<string, int> Examinations;
+    public KeyValuePair<string, int> AssignmentsAndProjects;
+    public KeyValuePair<string, int> Participation;
+    public KeyValuePair<string, int> Attendance;
+    public KeyValuePair<string, int> Behavior;
+    public KeyValuePair<string, int> EffortAndImprovement;
+}
