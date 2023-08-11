476a417fbfd90a10624f685a925c5af2059b79b7
Spas Milenkov <spasmilenkov123@gmail.com>
2023-08-11 19:26:19 +0300

    Add the contacts section to the homepage

--- UI/src/views/HomeView.vue
+++ UI/src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script lang="ts" setup>
 import FeaturesSectionComponent from '../components/landingPage/FeaturesSectionComponent.vue'
 import HeroSection from '../components/landingPage/HeroSectionComponent.vue'
+import ContactsSection from '../components/landingPage/ContactsSectionComponent.vue'
 </script>
 
 <template>
   <HeroSection></HeroSection>
   <FeaturesSectionComponent></FeaturesSectionComponent>
+  <ContactsSection />
 </template>
