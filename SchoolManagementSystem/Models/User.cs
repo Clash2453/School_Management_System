8e3fde97d5d99b13ebac489c5a3512349d2cd9c4
Spas Milenkov <spasmilenkov123@gmail.com>
2023-08-11 11:04:37 +0300

    Separate name into first and last name

--- SchoolManagementSystem/Models/User.cs
+++ SchoolManagementSystem/Models/User.cs
@@ -8,10 +8,12 @@ public class User
 {
     [Key]
     public int UserId { get; set; }
-    public string Name { get; set; } = null!;
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
     public string Email { get; set; } = null!;
     public string Role { get; set; } = null!;
     public string Password { get; set; } = null!;
     public string Salt { get; set; } = null!;
 
+    public string GetFullName() => $"{FirstName} {LastName}";
 }
